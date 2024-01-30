4344b35631249129665310ce4dfd2bbfc510d0f2
Robert Lopes <robertclopes@gmail.com>
2024-01-30 15:21:32 -0300

    feat: :sparkles: Criado função de soma com teste

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(soma(10, 15))
+}
+
+func soma(a int, b int) int {
+	return a + b
+}
---
+++ main_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "testing"
+
+func TestSoma(t *testing.T) {
+
+	total := soma(25, 35)
+
+	if total != 60 {
+		t.Errorf("Resultado da soma é inválido: Resultado %d. Esperado: %d", total, 60)
+	}
+}
