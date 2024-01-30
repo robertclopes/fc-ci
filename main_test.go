79da13d9a02c15a6126a5dc31b3bcb395b23f362
Robert Lopes <robertclopes@gmail.com>
2024-01-30 16:17:15 -0300

    fix: :bug: Corrigindo função soma no teste

--- main_test.go
+++ main_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestSoma(t *testing.T) {
 
-	total := Soma(25, 35)
+	total := soma(25, 35)
 
 	if total != 60 {
 		t.Errorf("Resultado da soma é inválido: Resultado %d. Esperado: %d", total, 60)
